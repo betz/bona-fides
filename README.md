ebd08df6f408341f748fb987c90751e07346cdf6
Tom Behets (betz) <behets@gmail.com>
2012-04-20 05:57:41 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,121 @@
-bona-fides
+Bona Fides
 ==========
 
 Mac webdevelopment setup script.
+This is my personal OSX Lion initialisation script, setting up many used tools.
+As i didn't find many scripts, and most people use Mamp or Webstart, i decided to build my own and share it.
+If you have suggestions, please create an issue on Github.
+
+What does Bona Fides install?
+=============================
+- CGG 10.7-v2 (without using XCode)
+- Git 1.7.9.4
+- Homebrew
+- Apache
+- PHP 5.4.0
+- Pear
+- MYSQL
+
+Only when a version is listed, i specifically chose that version. 
+Others versions may change.
+
+PHP extensions
+==============
+apc
+bcmath 
+bz2 
+calendar 
+Core 
+ctype 
+curl 
+date 
+dom 
+ereg 
+exif 
+fileinfo 
+filter 
+ftp gd 
+gettext 
+hash iconv 
+imap 
+intl 
+json 
+ldap 
+libxml 
+mbstring 
+mcrypt 
+memcache 
+memcached 
+mhash 
+mongo 
+mssql 
+mysql 
+mysqli 
+mysqlnd 
+OAuth 
+odbc 
+openssl 
+pcntl 
+pcre 
+PDO 
+pdo_dblib 
+pdo_mysql 
+pdo_pgsql 
+pdo_sqlite 
+pgsql 
+Phar 
+posix 
+Reflection 
+session 
+shmop 
+SimpleXML 
+soap 
+sockets 
+solr 
+SPL 
+SQLite 
+sqlite3 
+standard 
+sysvmsg 
+sysvsem 
+sysvshm 
+tidy 
+tokenizer 
+uploadprogress 
+wddx 
+xdebug 
+xhprof 
+xml 
+xmlreader 
+xmlrpc 
+xmlwriter 
+xsl 
+zip 
+zlib 
+Xdebug
+xslcache
+
+available but disabled by default: apc, xslcache
+
+How to?
+=======
+
+- On a fresh mac install, install git, See at following link to download the DMG. http://code.google.com/p/git-osx-installer/downloads/list
+- Open terminal.app
+- Clone the Github repo with following command: (just copy and paste in terminal and hit enter)
+ git clone https://betz@github.com/betz/bona-fides.git
+- When done, enter the folder you just created with cloning the repo. Do this with following command:
+ cd bona-fides
+- Make the setup.sh file executable with next command:
+ chmod +x setup.sh
+- Start the setup:
+ ./setup.sh
+
+When asked for your password, enter your mac user password and hit enter.
+It is normal that you dont see your cursor moving when typing a password.
+
+Credits
+=======
+For the PHP part, i am using the version of http://php-osx.liip.ch
+This script is maintained by Tom Behets (betz) behets@gmail.com
+
---
+++ setup.sh
@@ -0,0 +1,76 @@
+USER=$(whoami)
+
+
+# Download and install GCC for Mac without Xcode, if it doesn't exist already
+command -v gcc >/dev/null 2>&1 || {
+  echo "Downloading GCC for Mac, without Xcode."
+  test "$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://cloud.github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg)" != '200' && (echo "Error, something went wrong"; exit 0)
+  if [ "$?" == '0' ]; then
+    echo "Cannot find GCC for Mac, file not found."
+    exit 0
+  fi
+  curl -O http://cloud.github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg
+  echo "Installing GCC for Mac."
+  sudo installer -pkg GCC-*.pkg -target /
+}
+
+
+# Download and install git, if it doesnt exist already
+command -v git >/dev/null 2>&1 || {
+  echo "Downloading git for Mac."
+  curl -O http://git-osx-installer.googlecode.com/files/git-1.7.9.4-intel-universal-snow-leopard.dmg
+  hdiutil attach git*.dmg
+  echo "Installing git for Mac."
+  sudo installer -pkg '/Volumes/Git 1.7.9.4 Snow Leopard Intel Universal/git-1.7.9.4-intel-universal-snow-leopard.pkg' -target /
+  hdiutil detach /Volumes/Git*
+}
+
+
+# Install homebrew if it doesnt exist already
+command -v brew >/dev/null 2>&1 || {
+  echo "Installing Homebrew"
+  /usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+  brew doctor
+  brew update
+}
+
+
+# Install PHP 5.4.0
+echo "Installing PHP 5.4.0"
+curl -s http://php-osx.liip.ch/install.sh | bash -s 5.4
+echo "Creating symlink for PHP CLI"
+sudo rm /usr/bin/php
+sudo ln -s /usr/local/php5/bin/php /usr/bin/php
+
+# Aditional notes on this PHP installation: See http://php-osx.liip.ch/
+#
+# Included extensions
+# bcmath bz2 calendar Core ctype curl date dom ereg exif fileinfo filter ftp gd gettext hash iconv imap intl json ldap libxml mbstring mcrypt memcache memcached mhash mongo mssql mysql mysqli mysqlnd OAuth odbc openssl pcntl pcre PDO pdo_dblib pdo_mysql pdo_pgsql pdo_sqlite pgsql Phar posix Reflection session shmop SimpleXML soap sockets solr SPL SQLite sqlite3 standard sysvmsg sysvsem sysvshm tidy tokenizer uploadprogress wddx xdebug xhprof xml xmlreader xmlrpc xmlwriter xsl zip zlib Xdebug
+# available but disabled by default: apc, xslcache
+#
+# php.ini
+# The php.ini is located in /usr/local/php5/lib/php.ini, additional config files are located in /usr/local/php5/php.d/. The php.ini is based on php.ini-development
+#
+# apc and xslcache
+# apc and xslcache need to be enabled manually in /usr/local/php5/php.d/50-extension-apc.ini and /usr/local/php5/php.d/50-extension-xslcache.ini.
+#
+# xdebug
+# xdebug settings needs to be configured (idekey etc) manually in /usr/local/php5/php.d/50-extension-xdebug.ini.
+
+
+# Install Pear
+echo "Installing Pear"
+curl http://pear.php.net/go-pear.phar > go-pear.phar
+sudo php -d detect_unicode=0 go-pear.phar
+
+
+# Install MySQL5
+echo "Installing MySQL5"
+brew install mysql
+mysql_install_db --verbose --user=$USER --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
+mkdir -p ˜/Library/LaunchAgents
+cp /usr/local/Cellar/mysql/*/homebrew.mxcl.mysql.plist ˜/Library/LaunchAgents
+launchctl load -w ˜/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+
+
+
